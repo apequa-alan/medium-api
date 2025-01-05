5e436868e58b0c127d3c9c56d336338dacee6ea7
Apequa <apequa.alan@gmail.com>
2025-01-05 12:49:28 +0300

    Add type orm and db config

---
+++ src/ormconfig.ts
@@ -0,0 +1,12 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'john.db.elephantsql.com',
+  port: 5432,
+  username: 'zhdzmkms',
+  password: 'JMkTUtVEfNbLaa60YspC6zcD2W0yRVpU',
+  database: 'zhdzmkms',
+};
+
+export default config;
