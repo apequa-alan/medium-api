2e511227b512a4c97ce2db286933e7bcf643c65a
alanapekov <apequa.alan@gmail.com>
2025-04-05 14:25:14 +0300

    Add follow and unfollow logic for profile

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '@app/decorators/user.decorator';
 import { ProfileResponseInterface } from '@app/types/profileResponse.interface';
 import { ProfileService } from '@app/profile/profile.service';
+import { AuthGuard } from '@app/user/guards/auth.guard';
 
 @Controller('profiles')
 export class ProfileController {
@@ -13,6 +21,32 @@ export class ProfileController {
     @Param('username') username: string,
   ): Promise<ProfileResponseInterface> {
     const profile = await this.profileService.getProfile(
+      currentUserId,
+      username,
+    );
+    return this.profileService.buildUserProfile(profile);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followToProfile(
+    @User('id') currentUserId: number,
+    @Param('username') username: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.followToProfile(
+      currentUserId,
+      username,
+    );
+    return this.profileService.buildUserProfile(profile);
+  }
+
+  @Delete(':username/follow')
+  @UseGuards(AuthGuard)
+  async unFollowFromProfile(
+    @User('id') currentUserId: number,
+    @Param('username') username: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.unFollowFromProfile(
       currentUserId,
       username,
     );
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -3,19 +3,84 @@ import { UserEntity } from '@app/user/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ProfileType } from '@app/profile/types';
+import { FollowEntity } from '@app/profile/follow.entity';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
 
-  async getProfile(userId: number, username: string): Promise<ProfileType> {
+  async getProfile(
+    currentUserId: number,
+    username: string,
+  ): Promise<ProfileType> {
     const user = await this.userRepository.findOne({ where: { username } });
     if (!user) {
       throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
     }
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+
+    return {
+      ...user,
+      following:
+        follow?.followerId === currentUserId && follow?.followingId === user.id,
+    };
+  }
+
+  async followToProfile(
+    currentUserId: number,
+    username: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({ where: { username } });
+    if (!user) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'Follower and following cant be the same',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+    if (!follow) {
+      const createFollow = new FollowEntity();
+      createFollow.followerId = currentUserId;
+      createFollow.followingId = user.id;
+      await this.followRepository.save(createFollow);
+    }
+
+    return { ...user, following: true };
+  }
+
+  async unFollowFromProfile(
+    currentUserId: number,
+    username: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({ where: { username } });
+    if (!user) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'Follower and following cant be the same',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    await this.followRepository.delete({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
     return { ...user, following: false };
   }
 
